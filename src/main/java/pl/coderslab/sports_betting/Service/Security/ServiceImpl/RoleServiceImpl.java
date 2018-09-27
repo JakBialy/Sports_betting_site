51b45ac142ea71563e24460bc663ce3666eca12d
Jakub Bialy <bialyj1@gmail.com>
2018-09-27 13:24:42 +0200

    code cleaning in html files

--- src/main/java/pl/coderslab/sports_betting/Service/Security/ServiceImpl/RoleServiceImpl.java
+++ src/main/java/pl/coderslab/sports_betting/Service/Security/ServiceImpl/RoleServiceImpl.java
@@ -9,9 +9,6 @@ import pl.coderslab.sports_betting.Service.Security.Service.RoleService;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-
-    // COMMIT TEST
-
     private final RoleRepository roleRepository;
 
     /**
